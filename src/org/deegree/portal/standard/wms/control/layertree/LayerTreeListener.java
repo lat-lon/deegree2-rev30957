736d405722781bee7a87fb3a8941f9093140ff52
Lyn Goltz <goltz@lat-lon.de>
2014-08-11 16:48:13 +0200

    preserver xml entities

--- src/org/deegree/portal/standard/wms/control/layertree/LayerTreeListener.java
+++ src/org/deegree/portal/standard/wms/control/layertree/LayerTreeListener.java
@@ -171,15 +171,25 @@ public class LayerTreeListener extends AbstractListener {
 
     private String encodeWithHtmlEntity( String s ) {
         StringBuffer buf = new StringBuffer();
+        boolean isEntity = false;
         for ( int i = 0; i < s.length(); i++ ) {
             char c = s.charAt( i );
-            if ( c >= 'a' && c <= 'z' || c >= 'A' && c <= 'Z' || c >= '0' && c <= '9' ) {
+            if ( isEntity ) {
                 buf.append( c );
+                if ( c == ';' )
+                    isEntity = false;
             } else {
-                buf.append( "&#" + (int) c + ";" );
+                if ( c >= 'a' && c <= 'z' || c >= 'A' && c <= 'Z' || c >= '0' && c <= '9' ) {
+                    buf.append( c );
+                } else if ( c == '&' ) {
+                    isEntity = true;
+                    buf.append( c );
+                } else {
+                    buf.append( "&#" + (int) c + ";" );
+                }
             }
         }
         return buf.toString();
     }
-
+    
 }
